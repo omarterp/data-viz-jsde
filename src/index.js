501e0d7a5372623958ef4b86b56294e866a011e7
Eric Perales <omarterp@gmail.com>
2017-06-26 08:20:35 -0400

    integrating fetch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-import './index.css';
+// import './index.css';
+// import numeral from 'numeral';
+//
+// const courseValue = numeral(1000).format('$0,0.00');
+// // debugger
+// console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
 
-import numeral from 'numeral';
+import {getUsers} from './api/userApi';
 
-const courseValue = numeral(1000).format('$0,0.00');
-// debugger
-console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+// Populate table of users via API call.
+getUsers().then(result => {
+      let usersBody = "";
+
+      result.forEach(user => {
+        usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+      });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
